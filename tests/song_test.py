79a3f3e622e3fd28b94a1a052070c4a944db232d
Nixon2223 <n.nixon@hotmail.co.uk>
2021-11-27 15:51:23 +0000

    song get tests passed

--- tests/song_test.py
+++ tests/song_test.py
@@ -1,6 +1,13 @@
 import unittest
+from src.song import Song
 
 class TestSong(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.song = Song("We Are The Champions", "Queen")
+    
+    def test_get_name(self):
+        self.assertEqual("We Are The Champions", self.song.name)
+
+    def test_get_artist(self):
+        self.assertEqual("Queen", self.song.artist)
