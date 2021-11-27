5157475703bba762184a7ebdcb075767cccf3f3e
Nixon2223 <n.nixon@hotmail.co.uk>
2021-11-27 16:28:08 +0000

    add guest to room, passed

--- src/room.py
+++ src/room.py
@@ -4,3 +4,11 @@ class Room:
         self.capacity = capacity
         self.current_num_guests = 0
         self.guests = []
+        self.songs = []
+    
+    def guest_count(self):
+        return len(self.guests)
+    
+    def add_guest(self, guest):
+        self.guests.append(guest)
+
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,13 +1,22 @@
 import unittest
 from src.room import Room
+from src.guest import Guest
 
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
         self.room = Room(1, 100)
+        self.guest1 = Guest(100, "We Are The Champions")
     
     def test_get__number(self):
         self.assertEqual(1, self.room.number)
     
     def test_get_capacity(self):
         self.assertEqual(100, self.room.capacity)
+    
+    def test_check_guest_count(self):
+        self.assertEqual(0, self.room.guest_count())
+    
+    def test_add_guest(self):
+        self.room.add_guest(self.guest1)
+        self.assertEqual(1, self.room.guest_count())
