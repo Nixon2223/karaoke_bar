ffac2b8a0c5944f8b3c3b006d943ba5e828deed2
Nixon2223 <n.nixon@hotmail.co.uk>
2021-11-27 16:07:25 +0000

    karaoke bar tests passed

--- tests/karaoke_bar_test.py
+++ tests/karaoke_bar_test.py
@@ -1,6 +1,14 @@
 import unittest
 
+from src.karaoke_bar import Karaoke_Bar
+
 class TestKaraoke_Bar(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.karaoke_bar = Karaoke_Bar("Velvet Palace", 15)
+    
+    def test_get_name(self):
+        self.assertEqual("Velvet Palace", self.karaoke_bar.name)
+    
+    def test_get_entry(self):
+        self.assertEqual(15, self.karaoke_bar.entry_fee)
