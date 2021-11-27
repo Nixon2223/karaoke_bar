b52b72aa207948884a34647ed2375dffc7d500dc
Nixon2223 <n.nixon@hotmail.co.uk>
2021-11-27 15:58:01 +0000

    room get tests passed

--- tests/room_test.py
+++ tests/room_test.py
@@ -1,7 +1,13 @@
 import unittest
-
+from src.room import Room
 
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.room = Room(1, 100)
+    
+    def test_get__number(self):
+        self.assertEqual(1, self.room.number)
+    
+    def test_get_capacity(self):
+        self.assertEqual(100, self.room.capacity)
